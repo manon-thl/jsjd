f9b24a60e347336a28353539ef706be975ba59e0
thl <tanhongliang@qq.com>
2021-06-07 18:49:45 +0800

    项目搭建

---
+++ src/test/java/com/gari/jsjd/JsjdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gari.jsjd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsjdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
